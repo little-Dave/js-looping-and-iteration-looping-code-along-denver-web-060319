00f88f1e5572fb2a9d792cab923e8845e41abc0e
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-07-16 11:04:36 -0600

    Create a 'for' loop and a 'while' loop

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, event) {
+  let customMessages = [];
+  for (let i = 0; i < namesArray.length; i++) {
+    customMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+  }
+  return(customMessages);
+}
+
+function countdown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number --;
+  }
+}
